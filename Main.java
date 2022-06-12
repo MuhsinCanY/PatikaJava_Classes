a0b7a6fe1f72f580744f00515debaf3a3658a261
MuhsinCanY <96295315+MuhsinCanY@users.noreply.github.com>
2022-06-12 20:59:55 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package IncomeCalculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee kemal = new Employee("Kemal",2000,45,2000);
+
+        kemal.toStringg();
+    }
+}
